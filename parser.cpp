754154c3cb565b978774bebe888232f5baee4224
Katherine Whitlock <toroidalcode@gmail.com>
2013-04-16 19:13:21 -0400

    Finally compilation

--- parser.cpp
+++ parser.cpp
@@ -1,21 +1,21 @@
 #include <cstdio>
 #include <string>
-#include "ctoml/src/include/toml.h"
+#include "ctoml/src/include/ctoml.h"
+
 
 void setupToml(CToml* toml){
 	toml->open(".plcolors.toml");
 	toml->parse();
 }
-	
+/*
 int main(){
-	CToml toml;
-	setupToml(&toml);
-
-	printf("%d",  toml["PATH_BG"].as_int());
-	return 0;
-}
-
-
+    CToml toml;
+    toml.open(".plcolors.toml");
+    toml.parse();
+    CTomlValue val = toml.get("PATH_BG");
+    printf("%d\n", val.as_int());
+    return 0;
+}*/
 
 
 
